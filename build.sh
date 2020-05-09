647e85b72578ea1bc1d33629af01db7257b36319
Ryan Au <auryan898@gmail.com>
2020-05-09 23:09:55 +0000

    Add package to auto-generated java code

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-swig -c++ -java -outdir lib BrickPi3.i
+swig -c++ -java -package com.brickpi3.java -outdir lib/com/brickpi3/java BrickPi3.i
 g++ -c -fpic BrickPi3_wrap.cxx -I/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/include -I/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/include/linux
-g++ -shared BrickPi3_wrap.o -o lib/libbrickpi.so
+g++ -fpic -shared BrickPi3_wrap.o -o lib/libbrickpi.so
