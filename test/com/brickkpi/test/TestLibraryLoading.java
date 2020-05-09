c3699f56e95f37c042eb812fbcd17a9f196c096c
Ryan Au <auryan898@gmail.com>
2020-05-09 11:23:10 -0400

    Converted to eclipse project and create test jar

---
+++ test/com/brickkpi/test/TestLibraryLoading.java
@@ -0,0 +1,10 @@
+package com.brickkpi.test;
+
+import com.brickpi.java.BrickPi3;
+
+public class TestLibraryLoading {
+  public static void main(String[] args) {
+    BrickPi3 b = new BrickPi3();
+    b.detect();
+  }
+}
