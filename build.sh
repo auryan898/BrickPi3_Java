747b7f737f8be643795083ed7847dc3258a12dbf
Ryan Au <auryan898@gmail.com>
2020-05-10 17:33:57 +0000

    Added shebang to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 swig -c++ -java -package com.brickpi3.java -outdir lib/com/brickpi3/java BrickPi3.i
 g++ -c -fpic BrickPi3_wrap.cxx -I/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/include -I/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/include/linux
 g++ -fpic -shared BrickPi3_wrap.o -o lib/libbrickpi.so
