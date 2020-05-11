0d8aa357256c901be11875b36fe3afd389745460
Ryan Au <auryan898@gmail.com>
2020-05-10 21:15:33 -0400

    Started creating wrappers for the motors and ports

--- lib/com/brickpi3/hardware/PiMotor.java
+++ lib/com/brickpi3/hardware/PiMotor.java
@@ -1,25 +1,44 @@
 package com.brickpi3.hardware;
 
-import com.brickpi3.java.SWIGTYPE_p_int16_t;
-import com.brickpi3.java.SWIGTYPE_p_int32_t;
-import com.brickpi3.java.SWIGTYPE_p_int8_t;
-import com.brickpi3.java.SWIGTYPE_p_uint8_t;
+import com.brickpi3.java.SWIGTYPE_p_int;
+import com.brickpi3.java.SWIGTYPE_p_short;
+import com.brickpi3.java.SWIGTYPE_p_signed_char;
+import com.brickpi3.java.SWIGTYPE_p_unsigned_char;
+import com.brickpi3.java.brickpi;
 
 public class PiMotor {
-  protected SWIGTYPE_p_uint8_t MotorState;
-  protected SWIGTYPE_p_int8_t MotorPower;
-  protected SWIGTYPE_p_int32_t MotorPosition;
-  protected SWIGTYPE_p_int16_t MotorSpeed; // DPS degrees per second
-  private PiPort port;
+  protected SWIGTYPE_p_unsigned_char motorState;
+  protected SWIGTYPE_p_signed_char motorPower;
+  protected SWIGTYPE_p_int motorPosition = brickpi.new_intp();
+  protected SWIGTYPE_p_short motorSpeed = brickpi.new_shortp(); // DPS degrees per second
+  private PiMotorPort port;
   private PiBrick brick;
 
-  public PiMotor(PiPort port) {
+  public PiMotor(PiBrick brick, PiMotorPort port) {
+    if (brick == null || port == null)
+      throw new IllegalArgumentException("Cannot create motor from null values");
     this.port = port;
-    this.brick = port.getPiBrick();
+    this.brick = brick;
   }
 
-  public void update() {
-    
+  protected void update() {
+    brick.getBrickPi3().get_motor_status((short)port.getPortAddress(), motorState, motorPower, motorPosition, motorSpeed);
+  }
+  
+  public int getState() {
+    return 0;
+  }
+  
+  public int getPower() {
+    return 0;
+  }
+  
+  public int getPosition() {
+    return brickpi.intp_value(motorPosition);
+  }
+  
+  public int getSpeed() {
+    return brickpi.shortp_value(motorSpeed);
   }
 
 }
